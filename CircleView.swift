2b9910999297a1844e0d20d0c1be79c02fec1060
SagarNandha <sagarnandha11@gmail.com>
2015-02-13 18:18:08 +0530

    -

--- CircleView.swift
+++ CircleView.swift
@@ -12,7 +12,7 @@ class CircleView: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.clearColor()
+        self.backgroundColor = UIColor.clearColor() //
     }
     
     required init(coder aDecoder: NSCoder) {
